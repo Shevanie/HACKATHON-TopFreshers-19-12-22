9b53d5b703c378a8fd633d1f60683ae2e5006974
Shevanie <92170854+Shevanie@users.noreply.github.com>
2022-12-19 18:19:14 +0530

    Add files via upload

---
+++ Shevanie/js/index.js
@@ -0,0 +1,14 @@
+
+function log() {
+    window.location.href = "/dashboard.html";
+}
+function forpass() {
+    window.location.href = "/forpassword.html";
+}
+var user = document.getElementById("username").value;
+function user() {
+
+    var myuser = [user];
+
+
+}
